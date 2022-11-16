57412ed3d03bcad13bae93746be05e312bdf0200
capricorncd <capricorncd@qq.com>
2022-11-16 10:13:25 +0900

    lint

--- react-performance/src/components/Input/index.tsx
+++ react-performance/src/components/Input/index.tsx
@@ -8,7 +8,7 @@ interface InputProps {
 
 /**
  * 最简单的方案：内外两个状态，手动同步
- * 
+ *
  * 受控模式下存在两个问题:
  * 1.原子性：Child 内部状态的更新会比 Parent 组件晚一个渲染周期，存在 tearing 的问题
  * 2.性能：因为是在 useEffect 中通过 setState 来做的状态同步，所以会额外的触发一次渲染，存在性能问题
@@ -16,16 +16,16 @@ interface InputProps {
  * @returns React.FC<InputProps>
  */
 export const Input: React.FC<InputProps> = (props) => {
-  const { value, onChange } = props
+  const { value, onChange } = props;
   const [state, setState] = useState(value);
 
-  const isControlled = value !== undefined
+  const isControlled = value !== undefined;
 
   useEffect(() => {
     if (isControlled) {
-      setState(value)
+      setState(value);
     }
-  })
+  });
 
   // 如果组件此时处于受控模式，直接使用来自外部的状态
   // 即便状态的同步是存在延迟，但是 Child 组件所使用到的值一定是最新的
