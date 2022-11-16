a830eeb114b6b799ed5e0f83332f2b2c237e26d8
capricorncd <capricorncd@qq.com>
2022-11-16 12:12:28 +0900

    Add comments

--- react-performance/src/components/Input/index.tsx
+++ react-performance/src/components/Input/index.tsx
@@ -1,3 +1,17 @@
+/**
+ * A Controlled Component
+ * is one that takes its current value through props and notifies changes through callbacks like onChange. A parent component "controls" it by handling the callback and managing its own state and passing the new values as props to the controlled component. You could also call this a "dumb component".
+ * 
+ * An Uncontrolled Component
+ * is one that stores its own state internally, and you query the DOM using a ref to find its current value when you need it. This is a bit more like traditional HTML.
+ * 
+ * Controlled:
+ * <input type="text" value={value} onChange={handleChange} />
+ * 
+ * Uncontrolled:
+ * <input type="text" defaultValue="foo" ref={inputRef} />
+ * Use `inputRef.current.value` to read the current value of <input>
+ */
 import React from 'react';
 import { Options, usePropsValue } from './usePropsValue';
 
