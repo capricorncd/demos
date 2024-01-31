2381a1f3b1cb3d020e9b5a998283139d24c63d72
capricorncd <capricorncd@qq.com>
2024-01-31 11:22:08 +0900

    Add app-website-nuxt3

---
+++ app-website-nuxt3/nuxt.config.ts
@@ -0,0 +1,40 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  devtools: { enabled: true },
+  ssr: false,
+  app: {
+  head: {
+    title: 'School Circle - College student video social media platform',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1, user-scalable=no, maximum-scale=1, minimum-scale=1' },
+      { name: 'description', content: 'School Circle - College student video social media platform', hid: 'description' },
+      { name: 'keywords', content: 'School Circle, College student video social media platform' },
+      { name: 'format-detection', content: 'telephone=no' }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+    ],
+    script: [
+      // share install
+      { src: '//webapi.amap.com/maps?v=1.4.10&key=1d0e4420bc5b99f19c1ce23dc634fc28&plugin=AMap.Geocoder' },
+      { src: '//webapi.amap.com/ui/1.0/main.js?v=1.0.11' },
+      { src: 'https://cdn.jsdelivr.net/npm/particles.js@2.0.0/particles.min.js' },
+    ]
+  },
+},
+  // Global CSS (https://go.nuxtjs.dev/config-css)
+  css: [
+    '@/assets/scss/constants.scss',
+    '@/assets/scss/index.scss',
+    'swiper/swiper-bundle.css'
+  ],
+
+  // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
+  plugins: [
+    {
+      src: '@/plugins/swiper.js',
+      ssr: false
+    }
+  ],
+})
---
+++ app-website-nuxt3/pages/mobile.vue
@@ -0,0 +1,13 @@
+<template>
+  <MobilePage/>
+</template>
+
+<script>
+  import MobilePage from '../components/Mobile/index.vue'
+  export default {
+    components: {
+      MobilePage
+    }
+  }
+</script>
+
